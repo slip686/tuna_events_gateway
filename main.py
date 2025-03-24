80376e7702a079184435426727877233ab0aa726
Kirill <slip686@gmail.com>
2025-03-24 22:10:37 +0300

    init

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sentry_sdk
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
@@ -6,6 +7,11 @@ from fastapi.responses import PlainTextResponse
 from config import HOST, PORT
 from utils import create_topics, connect_routers
 import routers
+
+sentry_sdk.init(
+    dsn="https://61f234e115e13cf5f11a1c92185bc696@o4509033998909440.ingest.de.sentry.io/4509034287792208",
+    send_default_pii=True,
+)
 
 app = FastAPI(root_path='/events_gateway', docs_url='/docs', title='Шлюз событий',
               description='Сервис для публикации событий',
